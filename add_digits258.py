38b49d3c6bfa13ff4feab4263acd6290cac08a44
raghavgr94 <raghavgr94@gmail.com>
2018-01-11 15:00:43 -0800

    Add digits to get the digital root

---
+++ add_digits258.py
@@ -0,0 +1,31 @@
+"""
+Given a non-negative integer num, 
+repeatedly add all its digits 
+until the result has only one digit.
+
+For example:
+
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. 
+Since 2 has only one digit, return it.
+"""
+class Solution:
+    """ Solution """
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        while (num // 10 > 0):
+            sum = 0
+            while num > 0:
+                sum += num % 10
+                num = num // 10
+            num = sum
+        return num
+
+    def addDigits_2(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        return ((num - 1) % 9) + 1
