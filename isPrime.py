260868470f882778e554198e6f4c6539ee37f2f9
raghavgr94 <raghavgr94@gmail.com>
2018-02-09 00:12:43 -0800

    :heavy_check_mark: is_prime works properly now

--- isPrime.py
+++ isPrime.py
@@ -1,8 +1,12 @@
-def isPrime(self, num):
+def isPrime(num):
     if num== 2 or num == 3: return True
     if num%2==0 or num<2: return False
-    for i in range(3, int(num**0.5), 2):    # only odd numbers
-        if num%i == 0:
-            return False
+    if num > 3:
+        for i in range(1, int(num**0.5) + 1, 2):    # only odd numbers
+            if num%i == 0:
+                print("what " + str(i))
+                return False
     
     return True
+
+print(isPrime(9))
