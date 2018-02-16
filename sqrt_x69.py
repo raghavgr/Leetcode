3d353783aa7ff8744b040bfe492047634dc8bf6b
raghavgr94 <raghavgr94@gmail.com>
2018-02-16 13:48:37 -0800

    :construction: Checking outputs. Find int square root of given number

--- sqrt_x69.py
+++ sqrt_x69.py
@@ -19,6 +19,25 @@ Explanation: The square root of 8 is 2.82842...,
  the decimal part will be truncated.
 """
 class Solution:
+    def mySqrt(self, num):
+        """
+        Binary search.
+        :type x: int
+        :rtype: int
+        """
+        if num <= 0: return 0
+        if num == 1 or num == 2: return 1
+        low = 0
+        high = num - 1
+        while low < high:
+            mid = (low + high) // 2
+            if mid**2 <= num and (mid + 1)**2 > num:
+                return mid
+            if mid**2 > num:
+                high = mid
+            if mid**2 < num:
+                low = mid
+    
     def mySqrt2(self, x):
         """
         Brute force. Got time limit exceeded.
@@ -32,22 +51,7 @@ class Solution:
                 return i
             i += 1
         return curr
-    
-    def mySqrt(self, x):
-        """
-        Binary search.
-        :type x: int
-        :rtype: int
-        """
-        if x <= 0: return 0
-        if x == 1 or x == 2: return 1
-        low = 0
-        high = x - 1
-        while low < high:
-            mid = (low + high) // 2
-            if mid**2 <= x and (mid + 1)**2 > x:
-                return mid
-            if mid**2 > x:
-                high = mid
-            if mid**2 < x:
-                low = mid
+
+obj = Solution()
+print(obj.mySqrt(16))
+print(obj.mySqrt(11))
