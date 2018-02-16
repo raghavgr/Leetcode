f59679c9109ee010654cffeb2878b7ad0a4d9417
raghavgr94 <raghavgr94@gmail.com>
2018-02-16 13:39:31 -0800

    :heavy_check_mark: Check if guven number is a power of two

---
+++ power_of_two231.py
@@ -0,0 +1,18 @@
+"""
+Given an integer, write a function to determine if it is a power of two.
+"""
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n < 1:
+            return False
+        i = n
+        while i >= 2:
+            if i % 2 == 0:
+                i = i // 2
+            else:
+                return False
+        return True
