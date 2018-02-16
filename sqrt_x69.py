266d853a0ada78e6b6ff05f09afb072f7f09da9d
raghavgr94 <raghavgr94@gmail.com>
2018-02-15 18:23:33 -0800

    :horse_racing: Find square root using binary search

--- sqrt_x69.py
+++ sqrt_x69.py
@@ -19,7 +19,7 @@ Explanation: The square root of 8 is 2.82842...,
  the decimal part will be truncated.
 """
 class Solution:
-    def mySqrt(self, x):
+    def mySqrt2(self, x):
         """
         Brute force. Got time limit exceeded.
         :type x: int
@@ -33,3 +33,21 @@ class Solution:
             i += 1
         return curr
     
+    def mySqrt(self, x):
+        """
+        Binary search.
+        :type x: int
+        :rtype: int
+        """
+        if x <= 0: return 0
+        if x == 1 or x == 2: return 1
+        low = 0
+        high = x - 1
+        while low < high:
+            mid = (low + high) // 2
+            if mid**2 <= x and (mid + 1)**2 > x:
+                return mid
+            if mid**2 > x:
+                high = mid
+            if mid**2 < x:
+                low = mid
