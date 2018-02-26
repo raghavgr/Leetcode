915b540a5296f67e09bbe88740db219b1262bdf3
raghavgr94 <raghavgr94@gmail.com>
2018-02-25 18:45:09 -0800

    :zap: Cleaner code added

--- isPrime.py
+++ isPrime.py
@@ -1,9 +1,9 @@
 def isPrime(num):
-    if num== 2 or num == 3: return True
-    if num%2==0 or num<2: return False
+    if num == 2 or num == 3: return True
+    if num % 2 ==0 or num < 2: return False
     if num > 3:
         for i in range(1, int(num**0.5) + 1, 2):    # only odd numbers
-            if num%i == 0:
+            if num % i == 0:
                 return False
     
     return True
