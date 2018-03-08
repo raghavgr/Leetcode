450784a4cd2cd06fd301f8620f02ee5c054c0756
raghavgr94 <raghavgr94@gmail.com>
2018-03-07 19:01:59 -0800

    :construction: still another naive approach. need to improve it

--- ugly_number_ii_264.py
+++ ugly_number_ii_264.py
@@ -7,6 +7,31 @@ For example, 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence of the first 10 ugly
 Note that 1 is typically treated as an ugly number, and n does not exceed 1690.
 """
 class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        while num >= 2:
+            if num % 2 == 0: num = num // 2
+            elif num % 3 == 0: num = num // 3
+            elif num % 5 == 0: num = num // 5
+            else: return False
+        return num == 1
+    
+    def nthUglyNumber1(self, n):
+        initial = [1, 2, 3, 4, 5, 6, 8, 9, 10, 12]
+        primes = [7, 11]
+        i = 13
+        count = 10
+        if n <= 10:
+            return initial[n - 1]
+        while count < n:
+            if self.isUgly(i):
+                count += 1
+            i += 1
+        return i - 1
+    
     def nthUglyNumber_2(self, n):
         """
         Time Limit exceeded. 
